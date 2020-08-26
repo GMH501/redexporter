59d7b5cee42f210fedb14b149a739b99200ed40d
GMH501 <40721844+GMH501@users.noreply.github.com>
2020-08-26 18:16:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
-from random import randint
+from redminelib import Redmine
+import os
+
+redmine_url = os.getenv('REDMINE_URL')
+redmine_username = os.getenv('REDMINE_USER')
+redmine_password = os.getenv('REDMINE_PASSWORD')
 
 app = Flask(__name__)
 
+redmine = Redmine("http://redmine-devops-test.apps.ocp.ocp-lab.serverlr.local/", redmine_username="admin", redmine_password="password")
+
 @app.route("/metrics")
 def index():
-    return("numero_progetti {}".format(randint(0, 10)))
+    return("numero_progetti {}".format(len(redmine.project.all())))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8081, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
